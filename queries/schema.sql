82d680a1a18a9c0f188257bc1f3842e3b8f5a515
Jeffrey Purvis <purvisjd@gmail.com>
2021-08-03 16:10:12 -0400

    adding schema query to directory

---
+++ queries/schema.sql
@@ -0,0 +1,55 @@
+--Creating tables for PH-EmployeeDB
+Create table departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	primary key (dept_no),
+	Unique (dept_name)
+);
+
+Create table employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	primary key (emp_no)
+);
+
+Create table dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+
+Create table salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no)
+);
+
+Create table dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
+);
+
+Create table titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no)
+);
